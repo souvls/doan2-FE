125e9766ccd583d4fe42ea94cf34f5d9c896ed56
soulixai vilasit <107106046+souvls@users.noreply.github.com>
2024-06-25 22:04:27 +0700

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+eslint: {
+    ignoreDuringBuilds: true,
+},
     images: {
         //domains: ['localhost'], 
         domains:['doan2-be.onrender.com']
